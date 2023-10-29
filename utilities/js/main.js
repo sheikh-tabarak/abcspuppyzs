d1ac054acb769b90415997c31890a2566e659016
sheikh-tabarak <tabarakyaseen46@gmail.com>
2023-10-29 06:49:16 +0500

    1. Header designed - few changes 2. Folder structure setup - delete 11 files and update 1 file

---
+++ utilities/js/main.js
@@ -0,0 +1,50 @@
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
+function openNav() {
+  if (
+    /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+      navigator.userAgent
+    )
+  ) {
+    document.getElementById("mySidenav").style.width = "100%";
+  } else {
+    document.getElementById("mySidenav").style.width = "50%";
+  }
+  document.getElementById("background-Overlay").style.width = "100%";
+  // document.getElementById("background-Overlay").style.visibility="visible";
+  //   document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("background-Overlay").style.width = "0%";
+  //   document.getElementById("background-Overlay").style.visibility="hidden"
+}
+
+// Logo Slider [for brands]
+
+$(document).ready(function () {
+  $(".customer-logos").slick({
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    arrows: false,
+    dots: false,
+    pauseOnHover: false,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 4,
+        },
+      },
+      {
+        breakpoint: 520,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+    ],
+  });
+});
