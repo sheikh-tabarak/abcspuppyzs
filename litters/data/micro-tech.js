f74ad156b1b631abeb6587a383655aa8da921481
sheikh-tabarak <tabarakyaseen46@gmail.com>
2023-11-17 03:14:00 +0500

    chore: delete micro-teacup.html and puppy-tabs.html

---
+++ litters/data/micro-tech.js
@@ -0,0 +1,73 @@
+const Litters = [
+    {
+        mode: 0,
+        image: "public/images/litters/1080x-mother-lil-bit-and-father-zeus-present.jpg",
+        name: "Momma Kahala & Father Jove",
+        desc: "Micro Teacup Goldendoodles Are Our Smallest Variety, Averaging Less Than 10lbs. These Are Goldendoodles in a Pocket-Size Package. - Micro Teacup Goldendoodles Are Our Smallest Variety, Averaging Less Than 10lbs. These Are Goldendoodles in a Pocket-Size Package - Micro Teacup Goldendoodles Are Our Smallest Variety, Averaging Less Than 10lbs. These Are Goldendoodles in a Pocket-Size Package.",
+        type: "Teacup Goldendoodle",
+        status: "Early Reservations Open!",
+        expected: "Spring 2024",
+        goHome: "Weekend of November 17, 2023",
+
+        available: "Available",
+        avg_weight: "5-10lbs",
+
+        first_choice_male_price: '5,200',
+        males_price: '5,000',
+        first_choice_female_price: '5,400',
+        females_price: '5,200',
+
+        fname: "Zeus",
+        fweight: "8.5\"",
+        fheight: "8lbs",
+        fdna: "https://www.pawprintgenetics.com/pedigrees/dogs/details/24552/",
+        fofa: "https://ofa.org/advanced-search/?appnum=2397814/",
+        fimages: ["https://www.abcspuppyzs.com/assets/img/our-goldendoodle-dads/lord-zeus/720-lord-zeus-serious.jpg", "https://www.abcspuppyzs.com/assets/img/our-goldendoodle-dads/lord-zeus/720-lord-zeus-2-weeks.jpg", "https://www.abcspuppyzs.com/assets/img/our-goldendoodle-dads/lord-zeus/720-lord-zeus-meadows.jpg", "https://www.abcspuppyzs.com/assets/img/our-goldendoodle-dads/lord-zeus/720-lord-zeus-6-weeks.jpg"],
+
+        mname: "lil bit",
+        mweight: "7\"",
+        mheight: "8lbs",
+        mdna: "https://www.pawprintgenetics.com/pedigrees/dogs/details/26476/",
+        mofa: "https://ofa.org/advanced-search/?appnum=2398331/",
+        mimages: ["https://www.abcspuppyzs.com/assets/img/our-goldendoodle-moms/micro-teacup-goldendoodle/lil-bit/720-lil-bit-around-the-bend.jpg", "https://www.abcspuppyzs.com/assets/img/our-goldendoodle-moms/micro-teacup-goldendoodle/lil-bit/720-lil-bit-oh-so-presh.jpg", "https://www.abcspuppyzs.com/assets/img/our-goldendoodle-moms/micro-teacup-goldendoodle/lil-bit/720-destiny-hawk-chomp.jpg", "https://www.abcspuppyzs.com/assets/img/our-goldendoodle-moms/micro-teacup-goldendoodle/lil-bit/720-destiny-yum.jpg"],
+        previousLitter: [
+            {
+                image: "https://www.abcspuppyzs.com/assets/img/our-goldendoodle-moms/micro-teacup-goldendoodle/lil-bit/720-guri-6-weeks.jpg",
+                text: "Guri | 8 weeks old"
+            },
+            {
+                image: "https://www.abcspuppyzs.com/assets/img/our-goldendoodle-moms/micro-teacup-goldendoodle/lil-bit/720-kiona-9-weeks.jpg",
+                text: "Kiona | 8 weeks old"
+            },
+            {
+                image: "https://www.abcspuppyzs.com/assets/img/our-goldendoodle-moms/micro-teacup-goldendoodle/lil-bit/720-tala-9-weeks.jpg",
+                text: "Tala | 8 weeks old"
+            },
+            {
+                image: "https://www.abcspuppyzs.com/assets/img/our-goldendoodle-moms/micro-teacup-goldendoodle/lil-bit/720-lil-tut-standing-fetch-the-gr.jpg",
+                text: "Lil-tut | 8 weeks old"
+            }
+            , {
+                image: "https://www.abcspuppyzs.com/assets/img/our-goldendoodle-moms/micro-teacup-goldendoodle/lil-bit/720x-from-guri-came-fifi.jpg",
+                text: "Guri came Fifi | 1 year, 8 weeks old"
+            }, {
+                image: "https://www.abcspuppyzs.com/assets/img/our-goldendoodle-moms/micro-teacup-goldendoodle/lil-bit/720x-from-lil-lot-came-maddie.jpg"
+                ,
+                text: "Lil-Lot came Maddie | 1 year, 8 weeks old"
+            }, {
+                image: "https://www.abcspuppyzs.com/assets/img/our-goldendoodle-moms/micro-teacup-goldendoodle/lil-bit/720x-from-caela-came-sophie.jpg",
+                text: "Caela came Sophie |  1 year, 8 weeks old"
+            }, {
+                image: "https://www.abcspuppyzs.com/assets/img/our-goldendoodle-moms/micro-teacup-goldendoodle/lil-bit/720x-from-bain-came-oliver.jpg",
+                text: "Bain came Oliver | 1 year, 8 weeks old"
+            }
+        ],
+
+
+        puppies: [
+        ]
+
+    }];
+
+
+
